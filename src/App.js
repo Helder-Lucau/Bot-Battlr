3bf0a6f3c92f595c46e7d3aef0d7850d44685459
Helder <helderlucau@gmail.com>
2023-07-30 13:17:07 +0300

    using useEffect hook to fetch data from an external API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
+import React, {useEffect} from 'react';
 
 function App() {
+
+  useEffect(() => {
+    fetch("http://localhost:8001/bots")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    })
+  })
   return (
     <div className="App">
       <h1>Bot Battlr</h1>
